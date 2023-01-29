703dfe5b522c83faec0b38c8a2660f8d948117da
Syakirana28 <110509134+Syakirana28@users.noreply.github.com>
2023-01-29 17:12:38 +0700

    Add files via upload

---
+++ js-video-27/js/json.js
@@ -0,0 +1,24 @@
+let tblmenu = [
+    {
+    "idmenu" : 1,
+    "idkategori" : 1,
+    "menu" : "Apel",
+    "harga" : 3000,
+    "warna" : ["merah", "kuning", "hijau"],
+    "stok" : true,
+    "keterangan" : null
+},
+
+{
+    "idmenu" : 2,
+    "idkategori" : 1,
+    "menu" : "Anggur",
+    "harga" : 3000,
+    "warna" : ["merah", "kuning"],
+    "stok" : false,
+    "keterangan" : null
+}
+
+]
+
+console.log(tblmenu[0].menu);
